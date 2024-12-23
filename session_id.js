1618af92feefdd87b951e1226180dfcc802267d9
franco43-free <75459430+franco43-free@users.noreply.github.com>
2024-12-23 18:36:59 -0300

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&fIVTVaiJ#sSyXfvB0oVXUUPcsySwsZ1Z7UQarAZF65lFPpqbkzS8",
 };
